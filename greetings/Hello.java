a6db30ce755175310fc46534b574368a079df116
cambelem <cambelem@appstate.edu>
2017-02-05 23:16:23 -0500

    Allows a -me option

--- greetings/Hello.java
+++ greetings/Hello.java
@@ -1,23 +1,30 @@
 //package p1;
 
 public class Hello{
+   
+    String uname;
+    String compName;
     
-    public Hello(){
+    public Hello(String[] names){
+        if(names.length == 3){
+            this.uname = names[2];
+            this.compName = names[1];
+        } else if (names.length == 1 && !(names[0].equals("-me"))){
+            this.uname = names [0];
+            this.compName = null;
+        } else {
+            this.uname = "Eric";
+        }
     }
 
     public static void main(String[] args){
-        Hello h = new Hello();
-        h.evaluate(args);    
+        Hello h = new Hello(args);
+        h.evaluate();    
     }
 
-    public void evaluate(String[] args){
-        if(args.length == 1){
-            System.out.println("Working");
-            String name = args[0].substring(0,1).toUpperCase() + args[0].substring(1);
-            System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Hello, Eric!");    
-        }
+    public void evaluate(){
+        String name = uname.substring(0,1).toUpperCase() + uname.substring(1);
+        System.out.println("Hello, " + name + "!");
     }
 }
 
