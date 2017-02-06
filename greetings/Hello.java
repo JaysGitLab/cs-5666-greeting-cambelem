e697ec4d630407a2c0e8d4b1c05d3b8e2c96c055
cambelem <cambelem@appstate.edu>
2017-02-05 20:12:15 -0500

    Added a main.

--- greetings/Hello.java
+++ greetings/Hello.java
@@ -5,6 +5,11 @@ public class Hello{
     public Hello(){
     }
 
+    public static void main(String[] args){
+        Hello h = new Hello();
+        h.evaluate();
+    }
+
     public void evaluate(){
         System.out.println("Hello, Eric!");
     }
