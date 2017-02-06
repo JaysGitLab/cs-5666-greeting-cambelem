10efcc086795fe1795e04b8f5530cb12f78fd8b5
cambelem <cambelem@appstate.edu>
2017-02-05 20:18:43 -0500

    umm?

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+    public static void main(String[] args){
+        if(args.length == 1){
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            System.out.println("Hello, Eric!");    
+        }
+    }
+}
+
